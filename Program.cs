fa9a7a938f0d3a08b1415ee56c2174f9b1ec3f97
YoungsonMan <choi13570@naver.com>
2024-08-01 17:53:12 +0900

    [Add] Add graph and node checking code
    Made a matrix graph according to the screenshot and also the printer for the connnected node for each node.

--- Program.cs
+++ Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // matrixGraph 구현
+            bool[,] matrixGraph = new bool[8, 8]
+            {   //  0      1      2       3     4      5      6      7
+                {  true,  true, false, false,  true, false, false, false },
+                {  true,  true,  true,  true, false, false, false, false },
+                { false,  true,  true, false,  true, false,  true, false },
+                { false,  true, false,  true, false, false, false,  true },
+                {  true, false,  true, false,  true, false,  true, false },
+                { false, false, false, false, false,  true,  true, false },
+                { false, false,  true, false,  true,  true,  true, false },
+                { false, false, false,  true, false, false, false,  true }
+            };
+
+            // 각 노드마다 이어져 있는노드 표시
+            for (int from = 0; from < 8; from++)
+            {
+                Console.WriteLine($"{from}정점: ");
+                for (int to = 0; to < 8; to++)
+                {
+                    if (matrixGraph[from, to] == true)
+                    {
+                        Console.WriteLine($"\t{to} 정점");
+                    }
+                }
+            }
+            Console.WriteLine();
         }
     }
 }
